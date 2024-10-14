84ffa6f452656ae091b8c131f65ee679a317bf55
Le Phuc <phuc.le@savvycom.vn>
2024-10-14 08:48:22 +0700

    Thay id = ten

--- src/Server/DB/Layers/DTO/GameMatchTable.java
+++ src/Server/DB/Layers/DTO/GameMatchTable.java
@@ -6,22 +6,25 @@ import java.awt.*;
 import java.util.List;
 
 public class GameMatchTable {
-
+    private server.db.layers.BUS.PlayerBUS playerBUS;
     public void addGameMatchesToPanel(JPanel jPanel3, List<server.db.layers.DTO.GameMatch> matchList) {
         // Cột tiêu đề cho bảng
         String[] columnNames = {
-                "Match ID", "Player 1 ID", "Player 2 ID", "Started Time"
+                "Match ID", "Player 1", "Player 2", "Winner", "Started Time"
         };
-
         // Dữ liệu cho bảng
-        Object[][] data = new Object[matchList.size()][8]; // 8 cột tương ứng với các thuộc tính
-
+        playerBUS= new server.db.layers.BUS.PlayerBUS();
+        Object[][] data = new Object[matchList.size()][5]; // 8 cột tương ứng với các thuộc tính
         for (int i = 0; i < matchList.size(); i++) {
             server.db.layers.DTO.GameMatch match = matchList.get(i);
             data[i][0] = match.getId();
-            data[i][1] = match.getPlayerID1();
-            data[i][2] = match.getPlayerID2();
-            data[i][3] = match.getStartedTime();
+            int Id1= match.getPlayerID1();
+            data[i][1]=playerBUS.getById(Id1).getName();
+            int Id2= match.getPlayerID2();
+            data[i][2] = playerBUS.getById(Id2).getName();
+            int winnerId= match.getWinnerID();
+            data[i][3] = playerBUS.getById(winnerId).getName();
+            data[i][4] = match.getStartedTime();
         }
 
         // Tạo model cho bảng
